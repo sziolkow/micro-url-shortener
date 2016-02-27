8ae5ff79d50308aaf88927d67d3200810305e22b
Slawek <sziolkow@gmail.com>
2016-02-27 21:38:17 +0100

    new service

--- url-shortener-management-service/src/main/java/org/home/sziolkow/microservice/management/web/ServiceInstanceRestController.java
+++ url-shortener-management-service/src/main/java/org/home/sziolkow/microservice/management/web/ServiceInstanceRestController.java
@@ -1,4 +1,4 @@
-package org.home.sziolkow.microservice.eurekaclient;
+package org.home.sziolkow.microservice.management.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
